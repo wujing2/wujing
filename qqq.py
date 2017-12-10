2e8551896159b6ab265f8e39ad2c00dbbc373f5b
wujing <1425070747@qq.com>
2017-12-10 10:19:17 +0800

    111

--- qqq.py
+++ qqq.py
@@ -5,3 +5,6 @@ driver.get("http://10.135.78.4:8080/adminLogin.jsp")
 driver.find_element_by_id("loginUser").send_keys("BOMUSER")
 driver.find_element_by_id("password").send_keys("PASS1234")
 driver.find_element_by_id("btnLogin").click()
+
+
+
